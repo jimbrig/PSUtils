92f678a952e2de3e29a171b585218adbe64c6c0a
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2022-06-24 18:37:34 +0000

    updates

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -3,12 +3,13 @@ Properties {
     $PSBPreference.Build.CompileModule = $false
     $PSBPreference.Help.DefaultLocale = 'en-US'
     $PSBPreference.Test.OutputFile = 'out/testResults.xml'
-    # $PSBPreference.Test.CodeCoverage.Enabled = $true
-    # $PSBPreference.Test.ScriptAnalysisEnabled = $true
+    $PSBPreference.Test.CodeCoverage.Enabled = $false
+    $PSBPreference.Test.ScriptAnalysisEnabled = $false
+    $PSDefaultParameterValues = @{"Publish-Module:NuGetApiKey"='oy2j4zzf2mbv2v6u4jsqgjmseqxxyrbupof7g2awowxxma'}
 }
 
 Task Default -depends Test
 
-Task Test -FromModule PowerShellBuild -minimumVersion '0.6.1'
+Task Test -FromModule PowerShellBuild #-minimumVersion '0.6.1'
 
-# Task Build -FromModule PowerShellBuild -Version '0.1.0'
+Task Build -FromModule PowerShellBuild #-Version '0.1.0'
