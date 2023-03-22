c71d27ac34e34c2ade17a2060e85e11204665085
Jimmy Briggs <jimmy.briggs@pwc.com>
2023-03-22 12:40:47 -0400

    feat: enhance psakefile

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -3,12 +3,45 @@ Properties {
     $PSBPreference.Build.CompileModule = $false
     $PSBPreference.Help.DefaultLocale = 'en-US'
     $PSBPreference.Test.OutputFile = 'out/testResults.xml'
-    # $PSBPreference.Test.CodeCoverage.Enabled = $true
-    # $PSBPreference.Test.ScriptAnalysisEnabled = $true
+    $PSBPreference.Test.CodeCoverage.Enabled = $true
+    $PSBPreference.Test.ScriptAnalysisEnabled = $true
+    $PSBPreference.Build.CopyDirectories = @()
+
+    # Publish settings
+    if ($galleryApiKey) {
+        $PSBPreference.Publish.PSRepositoryApiKey = $galleryApiKey.GetNetworkCredential().password
+    }
+
 }
 
 Task Default -depends Test
 
 Task Test -FromModule PowerShellBuild -minimumVersion '0.6.1'
 
-# Task Build -FromModule PowerShellBuild -Version '0.1.0'
+Task InstallAct {
+    if (-not (Get-Command -name act -CommandType Application -ErrorAction SilentlyContinue)) {
+        if ($IsWindows) {
+            choco install act-cli
+        } elseif ($IsLinux) {
+            curl https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+        } elseIf ($IsMacOS) {
+            brew install nektos/tap/act
+        }
+    } else {
+        'act already installed'
+    }
+}
+
+Task TestGHAction -depends Build, InstallAct {
+    act -j test -P ubuntu-latest=nektos/act-environments-ubuntu:18.04
+}
+
+Task GenerateYAMLHelp -depends GenerateMarkdown {
+    If (-not (Get-Command New-YamlHelp -CommandType Function -ErrorAction SilentlyContinue)) {
+        Install-Module -Name platyPS -Repository PSGallery -Scope CurrentUser -Force
+    }
+    New-YamlHelp -Path './Docs/en-US' -OutputFolder './Docs/en-US' -Force
+}
+
+
+Task GetHelpPreview Get-HelpPreview -Path .\Docs\en-US\PSXLDevTools-help.xml
